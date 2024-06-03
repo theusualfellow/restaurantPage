067b231ac3cfdc0e0a8b3b6c8553c2ff3bb391a4
sandeep <sandeepsingh23ok@gmail.com>
2024-06-02 21:31:47 -0300

    Add config file

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins:[
+    new HtmlWebpackPlugin({
+        title: 'webpack 5',
+        filename: 'index.html',
+        inject: 'body',
+        template: './src/index.html'
+    })
+  ]
+};
