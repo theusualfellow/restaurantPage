bc9f1d3af9c9cf1b8d5ea549c10ec02c3604a0da
sandeep <sandeepsingh23ok@gmail.com>
2024-06-06 20:53:24 -0300

    Add finishing touches

--- src/about.js
+++ src/about.js
@@ -2,6 +2,7 @@ export function about(){
     function heading(){
         const chefsHeading = document.createElement('h1')
         chefsHeading.innerText = 'Meet the chefs'
+        chefsHeading.classList = "chefsHeading"
         return chefsHeading
     }
 
@@ -13,12 +14,12 @@ export function about(){
         return newImage
     }
     
-
     function text(){
         const someText = document.createElement('p')
         someText.innerText = `Alan the Alchemist and Freddie Gibbs together are unstoppable
         when it comes to cooking great food. They joined forces and has made Alfredo to what it 
         is today. It's their hard work and dedication that made Alfredo the sensation it is today`
+        someText.classList= "chefsText"
         return someText
     }
 
--- src/home.js
+++ src/home.js
@@ -1,4 +1,12 @@
 export function addHomeDiv(){
-    return `this is home div`
+
+    const homeText = `Welcome to Alfredo Restaurant. This fine establishment was 
+    established in 2022. We offer only the best cuisines from the land of
+    italy made from only the best of ingredients`
+    const paragraph = document.createElement("p")
+    paragraph.innerText = homeText
+    paragraph.classList = "homeParagraph"
+    
+    return paragraph
 
 }
