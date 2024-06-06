6b0c6795fcc946261a652a95b4bf2c45a15bc140
sandeep <sandeepsingh23ok@gmail.com>
2024-06-06 19:46:09 -0300

    Add image and text functions to append to body

--- src/about.js
+++ src/about.js
@@ -1,3 +1,18 @@
 export function about(){
-    return `This is our about section`
+    function image(){
+        const newImage = document.createElement("img")
+        newImage.src = '../chefs.jpg'
+        newImage.style.height = '270px'
+        newImage.style.width = '400px'
+        return newImage
+    }
+    
+
+    function text(){
+        const someText = document.createElement('p')
+        someText.innerText = 'hello'
+        return someText
+    }
+
+    return {image, text}
 }
