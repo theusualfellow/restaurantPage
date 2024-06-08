e55ec25ed4c9f7536f92357abb649c1cc4b16de8
sandeep <sandeepsingh23ok@gmail.com>
2024-06-07 21:18:06 -0300

    Enable watch mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  watch: true,
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/,
+    aggregateTimeout: 600,
+  },
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+     {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+    },
+    ],
   },
 
   plugins:[
